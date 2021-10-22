5676f8802b3785c99e20f08ffd9ca7a966f85688
Fbaldesamo <baldesamofranco@gmail.com>
2021-10-22 11:52:33 +0800

    kulang ng choice 3-display

--- Program.cs
+++ Program.cs
@@ -5,23 +5,15 @@ namespace Baldesamo_Franco_Lab_Act_3_1
 {
     internal class Program
     {
-        private class addstud
+        public class addstud
         {
-            
-            public static void AddStud()
+            public static int AddStud()
             {
                 try
                 {
-                    //open/creat the file text
-                    FileStream fs = new FileStream("Student.txt", FileMode.Create);
-
-                    //enable you to write in the text
-                    StreamWriter sw = new StreamWriter(fs);
-
-
                     Console.Write("Student Number: ");
                     long studnum = Int64.Parse(Console.ReadLine());
-                    
+
                     Console.Write("Student Name: ");
                     string studname = Console.ReadLine();
 
@@ -34,7 +26,13 @@ namespace Baldesamo_Franco_Lab_Act_3_1
                     Console.Write("Email address: ");
                     string stud_emai = Console.ReadLine();
 
-                    sw.WriteLine("Student Number: "+studnum);
+                    //open/creat the file text
+                    FileStream fs = new FileStream("Student.txt", FileMode.Create);
+
+                    //enable you to write in the text
+                    StreamWriter sw = new StreamWriter(fs);
+
+                    sw.WriteLine("Student Number: " + studnum);
                     sw.WriteLine("Student Name: " + studname);
                     sw.WriteLine("Student Degree: " + stud_deg);
                     sw.WriteLine("Student Gender: " + stud_gen);
@@ -43,17 +41,123 @@ namespace Baldesamo_Franco_Lab_Act_3_1
                     sw.Close();
                     fs.Close();
 
+                    Console.WriteLine("Student Added! ");
+                    Console.WriteLine("---------------------------");
+                    return 1;
                 }
-                catch
+                catch (Exception)
                 {
-                    Console.WriteLine("Invalid input");
-
-                    AddStud();
-                    Console.ReadKey();
+                    Console.WriteLine("error");
+                    return 0;
                 }
             }
         }
-        public static void Userchoice()
+
+        public static int Userchoice(int choice)
+        {
+            //add new student
+            if (choice == 1)
+            {
+                int a = addstud.AddStud();
+                return a;
+            }
+            else if (choice == 2)
+            {
+                //read the file
+                string readText = File.ReadAllText("Student.txt");
+                Console.WriteLine(readText);
+                Console.ReadKey();
+                return 1;
+            }
+            else if (choice == 3)
+            {
+                //number of male and female
+                return 1;
+            }
+            else if (choice == 4)
+            {
+                //will close the file
+                Console.WriteLine("Press Any key to exit");
+                Console.ReadKey();
+                return 1;
+            }
+            return 0;
+        }
+
+        private static void Main(string[] args)
+        {
+            int UserChoice_1;
+            int UserChoice_2;
+            
+            
+            do
+            {
+                //get user choice to make
+                //UserChoice_1 will hold the chosen value of the user
+                //validate the chosen numbers
+                UserChoice_1 = Userchoice();
+            } while (UserChoice_1 == 0);
+            do
+            {
+                //will go to the specified choice
+                //will repeat if error
+                UserChoice_2 = Userchoice(UserChoice_1);
+            } while (UserChoice_2 == 0);
+
+            
+            Console.WriteLine("Still want to continue?");
+            Console.WriteLine("Type \"YES\" or \"No\" ");
+            string answer = Console.ReadLine().ToUpper();
+            Console.Clear();
+            while (answer == "YES")
+            {
+                Redo();
+            }
+           
+                Console.WriteLine("Enter to exit");
+                Console.ReadKey();
+            
+
+
+            
+            
+        }
+        public static void Redo()
+        {
+
+            int UserChoice_1;
+            int UserChoice_2;
+
+
+            do
+            {
+                //get user choice to make
+                //UserChoice_1 will hold the chosen value of the user
+                //validate the chosen numbers
+                UserChoice_1 = Userchoice();
+            } while (UserChoice_1 == 0);
+            do
+            {
+                //will go to the specified choice
+                //will repeat if error
+                UserChoice_2 = Userchoice(UserChoice_1);
+            } while (UserChoice_2 == 0);
+            Console.Clear();
+            Console.WriteLine("Still want to continue?");
+            Console.WriteLine("Type \"YES\" or \"No\" ");
+            string answer = Console.ReadLine().ToUpper();
+            Console.Clear();
+
+            while (answer == "YES")
+            {
+                Redo();
+            }
+
+            Console.WriteLine("Enter to exit");
+            Console.ReadKey();
+
+        }
+        public static int Userchoice()
         {
             Console.WriteLine("1.Add new student");
 
@@ -62,46 +166,27 @@ namespace Baldesamo_Franco_Lab_Act_3_1
             Console.WriteLine("3.Number of Males/Females");
 
             Console.WriteLine("4.Exit");
-             
-            
 
-
-        }
-        private static void Main(string[] args)
-        {
-            //get user input
-            Userchoice();
             Console.Write("Choice: ");
+            try
+            {
+                int choice = Int16.Parse(Console.ReadLine());
+                //validate repetition the choices to make
+                while (choice > 4 || choice < 1)
+                {
+                    Console.WriteLine("Invalid input!");
 
-            int choice = Int16.Parse(Console.ReadLine());
-            while (choice > 4 || choice < 1)
-            {
-                Console.WriteLine("Invalid input!");
-                Userchoice();
+                    Userchoice();
 
-                choice = Int16.Parse(Console.ReadLine());
+                    choice = Int16.Parse(Console.ReadLine());
+                }
+                return choice;
             }
-
-            //add new student
-            if (choice == 1)
+            catch
             {
-                addstud.AddStud();
+                Console.WriteLine("Invalid Input!");
+                return 0;
             }
-            else if (choice == 2)
-            {
-                //read the file
-            }
-            else if (choice == 3)
-            {
-                //number of male and female
-            }
-            else if (choice == 4)
-            {
-                //will close the file
-                Console.WriteLine("Press Any key to exit");
-                Console.ReadKey();
-            }
-            
         }
     }
 }
